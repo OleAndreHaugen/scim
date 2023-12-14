f970dcac2a125b3f9a85bdaf6f958c39d2866fc5
Ole Andre <ole-andre.haugen@neptune-software.com>
2023-12-14 07:50:59 +0100

    Added filter for displayName for Group List

--- artifacts/Script/GroupsRead/content.js
+++ artifacts/Script/GroupsRead/content.js
@@ -31,7 +31,63 @@ if (req?.params?.id) {
 if (req.query.startIndex) options.skip = req.query.startIndex - 1;
 if (req.query.count) options.take = req.query.count;
 
-// Todo more filters
+// Filters
+if (req?.query?.filter) {
+    // For now only supporting 1 query
+    let filter = req.query.filter.split(" ");
+    let filterField = filter[0];
+    let filterOperator = filter[1].toLowerCase();
+    let filterValue = filter[2].replace(/"/g, "");
+
+    // filterField
+    switch (filterField) {
+        case "displayName":
+            filterField = "name";
+            break;
+
+        default:
+            break;
+    }
+
+    // filterValue
+    switch (filterValue) {
+        case "true":
+            if (filterField === "locked") {
+                filterValue = false;
+            } else {
+                filterValue = true;
+            }
+            break;
+
+        case "false":
+            if (filterField === "locked") {
+                filterValue = true;
+            } else {
+                filterValue = false;
+            }
+            break;
+
+        default:
+            break;
+    }
+
+    // filterOperator
+    switch (filterOperator) {
+        case "eq":
+            where[filterField] = filterValue;
+            break;
+
+        case "ne":
+            where[filterField] = Not(filterValue);
+            break;
+
+        default:
+            break;
+    }
+}
+
+
+// TODO more filters
 options.where = where;
 
 // Count User(s)
